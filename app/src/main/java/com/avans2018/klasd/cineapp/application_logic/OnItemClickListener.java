a357e42eea9a7cf8add2fc288088be8126c027c5
TJFSchoo <tjfschoo@avans.nl>
2018-03-26 18:17:58 +0200

    Fixed comment

--- app/src/main/java/com/avans2018/klasd/cineapp/application_logic/OnItemClickListener.java
+++ app/src/main/java/com/avans2018/klasd/cineapp/application_logic/OnItemClickListener.java
@@ -1,6 +1,6 @@
 package com.avans2018.klasd.cineapp.application_logic;
 
-// Interface voor RecyclerView klik logica
+// Interface voor RecyclerView klik logica. Standaard pakt Android Studio de AdapterView versie hiervan.
 public interface OnItemClickListener {
     void onItemClick(int position);
 }
