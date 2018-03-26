99fdcb06a3bf01e225a16f7d26867679d601ad40
TJFSchoo <tjfschoo@avans.nl>
2018-03-26 18:07:04 +0200

    Added Intent for receive logic in DetailActivity

--- app/src/main/java/com/avans2018/klasd/cineapp/presentation/DetailActivity.java
+++ app/src/main/java/com/avans2018/klasd/cineapp/presentation/DetailActivity.java
@@ -1,9 +1,12 @@
 package com.avans2018.klasd.cineapp.presentation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.avans2018.klasd.cineapp.R;
+import com.avans2018.klasd.cineapp.domain.Movie;
 
 // Activity voor detailscherm bij onItemClick in MovieListActivity
 public class DetailActivity extends AppCompatActivity {
@@ -11,7 +14,12 @@ public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG,"Gestart vanuit MovieListActivity.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        Movie clickedMovie = (Movie) intent.getSerializableExtra(MovieListActivity.CLICKED_MOVIE);
+
     }
 }
