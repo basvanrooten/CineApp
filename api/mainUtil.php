04d1efbc7eaafc87aa764be9e898a3a5f946e22c
Wouter Kodde <wouterk99@gmail.com>
2018-03-26 19:30:45 +0200

    - Changed mainUtil added new functions

--- api/mainUtil.php
+++ api/mainUtil.php
@@ -12,3 +12,29 @@ function redirect($url)
 {
     header('Location: '.$url);
 }
+
+function getApiKey(){
+    return "F9413B2193145D4F8B14943B88B98";
+}
+
+function checkValidApiKey(){
+    if (isset($_POST['key']) && !empty($_POST['key']) && $_POST['key'] == getApiKey()){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function checkFields($fields){
+    foreach ($fields as $field){
+        if(!isset($_POST[$field]) && !empty($_POST[$field])){
+            return false;
+        }
+    }
+    return true;
+}
+
+function getField($field){
+    return $_POST[$field];
+}
