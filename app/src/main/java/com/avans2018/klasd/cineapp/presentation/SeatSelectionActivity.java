445813e57a8fde884e5f2a6fba59f75320d960d2
TJFSchoo <tjfschoo@avans.nl>
2018-03-27 20:35:03 +0200

    Added SeatSelectionActivity & activity_seat_selection.xml design (30 seats atm)

---
+++ app/src/main/java/com/avans2018/klasd/cineapp/presentation/SeatSelectionActivity.java
@@ -0,0 +1,15 @@
+package com.avans2018.klasd.cineapp.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.avans2018.klasd.cineapp.R;
+
+public class SeatSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seat_selection);
+    }
+}
