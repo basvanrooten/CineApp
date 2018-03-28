a1fc9d17649f60208dccf1aaf50bf460eefe4d6f
TJFSchoo <tjfschoo@avans.nl>
2018-03-28 20:33:59 +0200

    Added RecyclerView to DetailActivity + ScheduleListAdapter for viewing schedules when clicking a movie

--- app/src/main/java/com/avans2018/klasd/cineapp/presentation_layer/DetailActivity.java
+++ app/src/main/java/com/avans2018/klasd/cineapp/presentation_layer/DetailActivity.java
@@ -4,25 +4,38 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.avans2018.klasd.cineapp.R;
+import com.avans2018.klasd.cineapp.application_logic_layer.MovieListAdapter;
+import com.avans2018.klasd.cineapp.application_logic_layer.OnItemClickListener;
+import com.avans2018.klasd.cineapp.application_logic_layer.ScheduleListAdapter;
 import com.avans2018.klasd.cineapp.domain_layer.Movie;
+import com.avans2018.klasd.cineapp.domain_layer.MovieSchedule;
 import com.squareup.picasso.Picasso;
 import com.avans2018.klasd.cineapp.util_layer.StringLimiter;
 
+import java.util.ArrayList;
+
 // Activity voor detailscherm bij onItemClick in MainActivity
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements OnItemClickListener {
     private final static String TAG = "DetailActivity";
     private Context mContext;
+    private ArrayList<MovieSchedule> scheduleList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private ScheduleListAdapter adapter = new ScheduleListAdapter(DetailActivity.this, scheduleList);
+    final static String CLICKED_SCHEDULE = "clickedSchedule";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, "onCreate() called.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
 
         // Intent met data van film vanuit MainActivity
         Intent intent = getIntent();
@@ -46,13 +59,30 @@ public class DetailActivity extends AppCompatActivity {
         TextView detailCommentContent = (TextView) findViewById(R.id.detailFilmDescriptionContent);
         detailCommentContent.setText(clickedMovie.getInfo());
 
-
         TextView detailFilmTimesHeader = (TextView) findViewById(R.id.detailFilmTimesHeader);
         TextView detailFilmTimesContent = (TextView) findViewById(R.id.detailFilmTimesContent);
+
+        // RecyclerView voor het weergeven van lijst van films
+        recyclerView = (RecyclerView) findViewById(R.id.scheduleRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(DetailActivity.this));
+        recyclerView.setAdapter(adapter);
+        adapter.setOnItemClickListener(DetailActivity.this);
+        adapter.notifyDataSetChanged();
 
         // Picasso voor invullen ImageView
         String imageUrl = clickedMovie.getImageUrl();
         Picasso.with(mContext).load(imageUrl).fit().centerInside().into(detailImageView);
+    }
 
+    @Override
+    public void onItemClick(int position) {
+        // Klik logica voor meegeven film en opstarten DetailActivity
+        Log.i(TAG, "onItemClick() called.");
+        Intent detailIntent = new Intent(this, TicketSelectionActivity.class);
+        MovieSchedule schedule = scheduleList.get(position);
+        detailIntent.putExtra(CLICKED_SCHEDULE, schedule);
+        startActivity(detailIntent);
+        Log.i(TAG, "Starting TicketSelectionActivity...");
     }
 }
