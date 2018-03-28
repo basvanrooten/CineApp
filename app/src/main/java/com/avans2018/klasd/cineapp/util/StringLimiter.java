da8a66fe60d12d5523d63fe287dcce937ab0af89
Baveltje01 <basvanrooten@me.com>
2018-03-28 14:11:49 +0200

    - Implemented StringLimiter
    For easy use within the UI to stop textviews from clipping in its container

---
+++ app/src/main/java/com/avans2018/klasd/cineapp/util/StringLimiter.java
@@ -0,0 +1,20 @@
+package com.avans2018.klasd.cineapp.util;
+
+/**
+ * Created by Bas van Rooten on 28-3-2018.
+ * AVANS HOGESCHOOL 2125132
+ */
+
+public class StringLimiter {
+
+    public static String limit(String input, int length) {
+        StringBuilder stringBuilder = new StringBuilder(input);
+
+        if (stringBuilder.length() > length) {
+            stringBuilder.setLength(length - 4);
+            stringBuilder.append(" ...");
+        }
+
+        return stringBuilder.toString();
+    }
+}
