b4cb966c91df12678d59799dcdf49698eed9134b
TJFSchoo <tjfschoo@avans.nl>
2018-04-03 15:24:35 +0200

    String commit + Added FailedPaymentActivity

---
+++ app/src/main/java/com/avans2018/klasd/cineapp/presentation_layer/FailedPaymentActivity.java
@@ -0,0 +1,15 @@
+package com.avans2018.klasd.cineapp.presentation_layer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.avans2018.klasd.cineapp.R;
+
+public class FailedPaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_failed_payment);
+    }
+}
