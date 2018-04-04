4e491440704d134e8da0a7fa34fe353b45a6dec4
Jorrit Meeuwissen <jorritm076@gmail.com>
2018-04-04 19:41:17 +0200

    Seat gets free after 15 minutes if the order is not completed

--- api/calls/getSeatsByScheduleId.php
+++ api/calls/getSeatsByScheduleId.php
@@ -7,11 +7,52 @@ use Database\Database;
 $fields = array("scheduleId");
 $seats = array();
 
+date_default_timezone_set('Europe/Amsterdam');
+
 if(checkFields($fields) && checkValidApiKey()) {
 
     $scheduleId = getField($fields[0]);
+    $results = getSeats($scheduleId);
 
-    $statement = Database::getConnection()->prepare("SELECT m.TheaterId, s.Id, s.SeatNumber, s.RowNumber, t.Taken FROM MovieSchedule m JOIN Seat s ON m.TheaterId = s.TheaterId JOIN Taken t ON t.SeatId = s.Id && m.id = t.MovieScheduleId WHERE m.Id = ?");
+
+
+    foreach($results as $result){
+        $updateDate = new DateTime($result["UpdateDate"]);
+        $diff = $updateDate->diff(new DateTime());
+
+        if($diff->i >= 15 && $result["Taken"] == 1) {
+            $statement = Database::getConnection()->prepare("Update Taken Set Taken = 0 WHERE SeatId = ? AND MovieScheduleId = ?");
+            $statement->bindValue(1, $result["Id"], Database::PARAM_STR);
+            $statement->bindValue(2, $scheduleId, Database::PARAM_STR);
+
+            try
+            {
+                $statement->execute();
+            }
+            catch (\Exception $e)
+            {
+
+            }
+            $results = getSeats($scheduleId);
+        }
+        $MovieSchedule = array();
+
+        $movieSchedule["theaterId"] = $result["TheaterId"];
+        $movieSchedule["seatId"] = $result["Id"];
+        $movieSchedule["seatNumber"] = $result["SeatNumber"];
+        $movieSchedule["rowNumber"] = $result["RowNumber"];
+        $movieSchedule["taken"] = $result["Taken"];
+        $movieSchedule["lastUpdated"] = $result["UpdateDate"];
+
+        $seats["results"][] = $movieSchedule;
+    }
+
+
+
+}
+
+function getSeats($scheduleId) {
+    $statement = Database::getConnection()->prepare("SELECT m.TheaterId, s.Id, s.SeatNumber, s.RowNumber, t.Taken, t.UpdateDate FROM MovieSchedule m JOIN Seat s ON m.TheaterId = s.TheaterId JOIN Taken t ON t.SeatId = s.Id && m.id = t.MovieScheduleId WHERE m.Id = ?");
     $statement->bindValue(1, $scheduleId, Database::PARAM_STR);
 
     try
@@ -20,27 +61,10 @@ if(checkFields($fields) && checkValidApiKey()) {
     }
     catch (\Exception $e)
     {
-        //Return just the empty array.
-        //Do a die(); method so the code stops running and doesn't crash, because the statement->fetchAll() will throw another exception.
-        die(json_encode($seats));
+        return [];
     }
 
-    $results = $statement->fetchAll();
-
-    foreach($results as $result){
-        $MovieSchedule = array();
-
-        $movieSchedule["theaterId"] = $result["TheaterId"];
-        $movieSchedule["seatId"] = $result["Id"];
-        $movieSchedule["seatNumber"] = $result["SeatNumber"];
-        $movieSchedule["rowNumber"] = $result["RowNumber"];
-        $movieSchedule["taken"] = $result["Taken"];
-
-        $seats["results"][] = $movieSchedule;
-    }
-
-
-
+    return $statement->fetchAll();
 }
 
 echo json_encode($seats);
